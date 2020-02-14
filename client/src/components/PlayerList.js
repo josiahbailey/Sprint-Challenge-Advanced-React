81a81d0d96a9a82a18121ccd195f2aff20e9eeb3
Josiah <reikiryo@yahoo.com>
2020-02-14 12:29:24 -0500

    useStyledCard hook dysfunctional

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PlayerCard from './PlayerCard'
+import useStyledCard from '../hooks/useStyledCard'
 
 const PlayerList = ({players}) => {
+    const [card, setCardData] = useStyledCard(1)
     return (  
         <div className='list'>
-            {players.map(player => (
-                <PlayerCard player={player}/>
-            ))}
+            {players.map(player => {
+                // <PlayerCard player={player}/>
+                setCardData(player)
+                return card
+        })}
         </div>
     );
 }
